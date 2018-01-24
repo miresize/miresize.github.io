fac68e0504f358c34bee27f43a4d093a5b1e5335
ali.abbas <ali.abbas@tenpearls.com>
2018-01-24 17:38:53 +0500

    autotextview bug fixed

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,9 +1,10 @@
 function pickImage(cb) {
     var input = $(document.createElement('input'));
     input.attr('type','file');
-    input.attr('accept','image/jpeg, image/png');
+    input.attr('accept','image/jpeg,image/jpg,image/png');
     input.trigger('click');
     input.change(function (event) {
+        console.log(input.val());
         var tgt = event.target || window.event.srcElement;
         var files = tgt.files;
         cb(files[0]);
